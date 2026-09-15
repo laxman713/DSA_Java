5fb01f6c8afed56a5fd0d334554d94f119bfb5e9
Laxman <laxmanbhuvanagiri6@email.com>
2026-09-15 10:30:35 +0530

    Valid Parenthesis

---
+++ Stacks/validParenthesis.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+class validParenthesis{
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+
+                st.push(ch);
+
+            } else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.peek();
+
+                if (ch == ')' && top == '(') {
+                    st.pop();
+                }
+                else if (ch == '}' && top == '{') {
+                    st.pop();
+                }
+                else if (ch == ']' && top == '[') {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "()[]{}";
+
+        boolean result = isValid(s);
+
+        System.out.println(result);
+    }
+}
